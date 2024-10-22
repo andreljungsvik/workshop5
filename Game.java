0cdef90618887f5f28652dd03a6127fe11a6f0e7
andreljungsvik <andre.ljungsvik@gmail.com>
2024-10-22 10:02:47 +0200

    add different responses

--- Game.java
+++ Game.java
@@ -1,3 +1,4 @@
+import java.util.Random;
 import java.io.*;
 
 public class Game {
@@ -8,11 +9,14 @@ public class Game {
 		try {
 			br = new BufferedReader(new FileReader("continents.csv"));
 			String line;
+			int correct = 0;
+			Random r = new Random();
 			while ((line = br.readLine()) != null) {
 				String[] cols = line.split(",");
 				String country = cols[0];
 				String continent = cols[1];
 				String answer = null;
+				
 				do {
 					answer = console.readLine("Which continent does " + country + " belong to? ");
 					if (answer.equalsIgnoreCase("help")) {
@@ -20,8 +24,18 @@ public class Game {
 					}
 				} while (answer.equalsIgnoreCase("help"));
 				if (answer.equalsIgnoreCase(continent)) {
-					System.out.println("Correct!");
-				} else {
+					switch(r.nextInt() % 3){
+					 case 0:
+						   System.out.println("Well done!");
+						   break;
+					 case 1:
+						   System.out.println("That is right!");
+						   break;
+					 case 2:
+						   System.out.println("Correct!");
+					}
+					 correct++;
+				 }else {
 					System.out.println("No, the answer is " + continent);
 				}
 			}
